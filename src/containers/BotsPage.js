6060a699e8547360c70aaae5ad60d947a9da4634
gillsinaglass <rdgill95@gmail.com>
2019-03-20 10:32:50 -0400

    Created a search bar, feels pointless

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import BotCollection from "./BotCollection"
 import YourBotArmy from "./YourBotArmy"
 import BotSpecs from "../components/BotSpecs"
+import SearchBar from '../components/SearchBar'
 
 class BotsPage extends React.Component {
   constructor(){
@@ -9,7 +10,8 @@ class BotsPage extends React.Component {
     this.state = {
       allBots: [],
       myBots: [],
-      selectedBot: null
+      selectedBot: null,
+      searchTerm: ""
     }
   }
 
@@ -36,7 +38,7 @@ handleRemoveBot = (botObj) => {
 }
 
 getUnAsBots=()=>{
-    return this.state.allBots.filter(bot => !this.state.myBots.includes(bot))
+    return this.state.allBots.filter(bot => !this.state.myBots.includes(bot) && bot.name.includes(this.state.searchTerm))
   }
 
   handleBotSelect = (botObj) =>{
@@ -51,10 +53,18 @@ getUnAsBots=()=>{
     })
   }
 
+//Building a Search Bar
+  onSearch = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <YourBotArmy bots={this.state.myBots} handleBotClick={this.handleRemoveBot}/>
+        <SearchBar onSearch={this.onSearch} text={this.state.searchTerm}/>
         {this.state.selectedBot=== null ? <BotCollection bots={this.getUnAsBots()} handleBotClick={this.handleBotSelect} /> :
       <BotSpecs bot={this.state.selectedBot} goBack={this.goBack} handleBotClick={this.handleAddBot}/>}
       </div>
